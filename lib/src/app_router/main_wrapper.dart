8d44e3ad265c397b11c0213e692bd42143ecf677
CreeperFarm <creeperfarm.pro@gmail.com>
2024-06-16 03:01:15 +0200

    Update

--- lib/src/app_router/main_wrapper.dart
+++ lib/src/app_router/main_wrapper.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mymangatheque/src/const/own_icon.dart';
 import 'package:mymangatheque/src/const/theme.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:glass_kit/glass_kit.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -19,8 +20,23 @@ class MainWrapper extends ConsumerStatefulWidget {
   ConsumerState<MainWrapper> createState() => _MainWrapperState();
 }
 
+List<String> navIcons = [
+  "home",
+  "collection",
+  "search",
+  "calendar",
+  "user"
+];
+
+List<String> navTitle = [
+  "Accueil",
+  "Collection",
+  "Recherche",
+  "Planning",
+  "Profil"
+];
+
 class _MainWrapperState extends ConsumerState<MainWrapper> {
-  final int selectedItemColor = 0xFF1783a5;
 
   int selectedIndex = 0;
 
@@ -71,18 +87,81 @@ class _MainWrapperState extends ConsumerState<MainWrapper> {
           widget.navigationShell,
           Align(
             alignment: Alignment.bottomCenter,
-            child: navBar(),
+            child: navBar(textColor),
           ),
         ],
       ),
     );
   }
 
-  Widget navBar() {
-    return Container(
+  Widget navBar(unSelectedColor) {
+    Color? selectedColor = Colors.cyanAccent;
+    if (unSelectedColor == Color(0xFF1c1b1f)) {
+      selectedColor = Colors.blueAccent[700];
+    } else {
+      selectedColor = Colors.cyanAccent;
+    }
+    return GlassContainer.clearGlass(
+      gradient: LinearGradient(
+        colors: [
+          Colors.white.withOpacity(0.1),
+          Colors.white.withOpacity(0.1),
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ),
       height: 60,
       margin: const EdgeInsets.only(bottom: 16, left: 16, right: 16),
-
+      borderColor: Colors.transparent,
+      borderRadius: const BorderRadius.all(Radius.circular(20)),
+      shadowColor: Colors.black.withOpacity(0.2),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: navIcons.map((iconName) {
+          int index = navIcons.indexOf(iconName);
+          bool isSelected = selectedIndex == index;
+          return Material(
+            color: Colors.transparent,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                  _goBranch(index);
+                });
+              },
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 0,
+                      right: 22,
+                      left: 22,
+                    ),
+                    child: OwnIcon(
+                      iconName: iconName,
+                      iconColor: isSelected
+                          ? selectedColor
+                          : unSelectedColor,
+                    ),
+                  ),
+                  Text(
+                    navTitle[index],
+                    style: TextStyle(
+                      color: isSelected
+                          ? selectedColor
+                          : unSelectedColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
