dafd352bc0d986d38b3967b3c096ec8a5d89d8d1
CreeperFarm <creeperfarm.pro@gmail.com>
2025-01-05 01:28:34 +0100

    Update scan_ean_page.dart

--- lib/src/front/page/scan_ean_page.dart
+++ lib/src/front/page/scan_ean_page.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mymangatheque/src/back/services/pocketbase.dart';
 import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
 class ScanEanPage extends ConsumerStatefulWidget {
@@ -32,46 +34,60 @@ class _ScanEanPageState extends ConsumerState<ScanEanPage> {
         child: Text("Malheureusement, le scan EAN n'est pas disponible pour le moment."),
       ),
     );*/
-    if (Platform.isAndroid || Platform.isIOS) {
-      dynamic ean;
-
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Scan EAN"),
-          backgroundColor: Colors.transparent,
-        ),
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                var res = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SimpleBarcodeScannerPage(),
-                  ),
-                );
-                setState(() {
-                  if (res is String) {
-                    ean = res;
-                  }
-                });
-              },
-              child: const Text('Open Scanner'),
-            ),
-            Text(ean ?? "No data"),
-          ],
+    if (!PocketBaseConnector().isLoggedIn()) {
+      context.go('/profile/signin');
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
       );
     } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Scan EAN"),
-          backgroundColor: Colors.transparent,
-        ),
-        body: Center(
-          child: Text("Platform not supported"),
-        ),
-      );
+      if (Platform.isAndroid || Platform.isIOS) {
+        dynamic ean;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Scan EAN"),
+            backgroundColor: Colors.transparent,
+          ),
+          body: Column(
+            // TODO: Add the code to add the scanned EAN to the database
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  var res = await SimpleBarcodeScanner.scanBarcode(
+                    context,
+                    barcodeAppBar: const BarcodeAppBar(
+                      enableBackButton: true,
+                      backButtonIcon: Icon(Icons.arrow_back_ios),
+                    ),
+                    isShowFlashIcon: true,
+                    delayMillis: 2000,
+                    cameraFace: CameraFace.front,
+                  );
+                  setState(() {
+                    if (res is String) {
+                      ean = res;
+                    }
+                  });
+                },
+                child: const Text('Open Scanner'),
+              ),
+              Text(ean ?? "No data"),
+            ],
+          ),
+        );
+      } else {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Scan EAN"),
+            backgroundColor: Colors.transparent,
+          ),
+          body: Center(
+            child: Text("Platform not supported"),
+          ),
+        );
+      }
     }
   }
 }
