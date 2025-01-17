1ed0a8744be6f16b0e72c53908fae3d5a05913c1
CreeperFarm <creeperfarm.pro@gmail.com>
2025-01-17 12:37:27 +0100

    Modifying theme to add text theme

--- lib/src/const/theme/dark_mode.dart
+++ lib/src/const/theme/dark_mode.dart
@@ -6,12 +6,128 @@ ThemeData darkMode = ThemeData(
     onSurface: Colors.grey.shade200,
     primary: const Color.fromARGB(255, 255, 255, 255),
     onPrimary: Colors.grey.shade900,
-    primaryContainer: Colors.grey.shade800, // Background color of the container
-    onPrimaryContainer: Colors.grey.shade200, // Text/Icon color in the container
+
+    // Background color of the container
+    primaryContainer: Colors.grey.shade800,
+
+    // Text/Icon color in the container
+    onPrimaryContainer: Colors.grey.shade200,
     secondary: const Color.fromARGB(255, 30, 30, 30),
     onSecondary: Colors.grey.shade400,
     tertiary: const Color.fromARGB(255, 47, 47, 47),
     onTertiary: Colors.white10,
-    inversePrimary: Colors.grey.shade300
+    inversePrimary: Colors.grey.shade300,
+  ),
+  textTheme: TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
+  ),
+  primaryTextTheme: TextTheme(
+    headlineLarge: TextStyle(
+      fontSize: 40,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
   ),
 );
--- lib/src/const/theme/light_mode.dart
+++ lib/src/const/theme/light_mode.dart
@@ -2,17 +2,134 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
-    surface: Colors.grey.shade200,
     // Color of the container/button and appBar
-    onSurface: Colors.black87,
+    surface: Colors.grey.shade200,
+
     // Color of the text of the main page
-    primary: Colors.grey.shade900,
+    onSurface: Colors.black87,
+
     // Color of the text/icon of the button and of the background of floatingActionButton
-    onPrimary: Colors.grey.shade100,
+    primary: Colors.grey.shade900,
+
     // Color of the text/icon of floatingActionButton
+    onPrimary: Colors.grey.shade100,
+    primaryContainer: Colors.grey.shade200,
+    onPrimaryContainer: Colors.grey.shade700,
     secondary: Colors.grey.shade200,
     onSecondary: Colors.blueGrey.shade900,
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade900,
+  ),
+  textTheme: TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
+  ),
+  primaryTextTheme: TextTheme(
+    headlineLarge: TextStyle(
+      fontSize: 40,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.grey.shade200,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.grey.shade200,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 15,
+      color: Colors.grey.shade200,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 10,
+      color: Colors.grey.shade200,
+    ),
   ),
 );
